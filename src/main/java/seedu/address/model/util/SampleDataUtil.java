39736ec016e2abd1332dd72c48c79d957ad831d0
FestiveCat <31hanyang@gmail.com>
2022-03-11 02:19:48 +0800

    [REFAC] Fix styling issues, especially for imports
    Fix styling issue that arose from changing Address into Mod.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.ContactList;
 import seedu.address.model.ReadOnlyContactList;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Mod;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
