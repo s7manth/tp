81c40a7d065a244f4e25c3fa06925cede4802a32
Cyolune <jan.alfenson.tan@gmail.com>
2022-03-22 03:41:50 +0800

    [FEAT] Add support for Undo in parser

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -4,20 +4,21 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-import java.lang.reflect.UndeclaredThrowableException;
-
 import static java.util.Objects.requireNonNull;
 
 /**
  * Undoes the previous command that commits to contact list.
  */
 public class UndoCommand extends Command {
+    public static final String COMMAND_WORD = "undo";
+
     public static final String MESSAGE_EARLIEST_VERSION = "The contact list is already in its earliest version";
     public static final String MESSAGE_SUCCESS = "Contact List changed to previous version";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
         if (model.isEarliestContactListVersion()) {
             throw new CommandException(MESSAGE_EARLIEST_VERSION);
         }
@@ -25,6 +26,7 @@ public class UndoCommand extends Command {
         model.undoContactList();
 
         return new CommandResult(MESSAGE_SUCCESS);
+
     }
 
     @Override
