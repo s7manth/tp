53baf57babec52445697db5334c963900a7707f8
Cyolune <jan.alfenson.tan@gmail.com>
2022-03-25 03:43:11 +0800

    [FEAT] Add tests for Undo and Redo commands

---
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+class UndoCommandTest {
+    @Test
+    public void execute_undo_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        model.addPerson(ALICE);
+        model.commitContent();
+        assertCommandSuccess(new UndoCommand(), model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_undo_fail() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new UndoCommand(), model, UndoCommand.MESSAGE_EARLIEST_VERSION);
+    }
+}
