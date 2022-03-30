df1b1e737edbd4d3e7edc365a15033f1008e4d70
s7manth <syalamarty002@gmail.com>
2022-03-31 01:20:41 +0800

    add example usages to redo and undo commands

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -9,7 +9,8 @@ public class RedoCommand extends Command {
     public static final String COMMAND_WORD = "redo";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Reverts the person list or task list to a later "
-            + "state, which exists after an undo command was executed.";
+            + "state, which exists after an undo command was executed.\n"
+            + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_LATEST_VERSION = "This is the latest version";
     public static final String MESSAGE_SUCCESS = "Contents have been changed to the next version";
--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -12,7 +12,8 @@ public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Reverts the person list or task list to a previous state, before an "
-            + "add/delete/edit person or newtask/deltask command was executed";
+            + "add/delete/edit person or newtask/deltask command was executed. \n"
+            + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_EARLIEST_VERSION = "There are no versions before this";
     public static final String MESSAGE_SUCCESS = "Contents have been changed to previous version";
